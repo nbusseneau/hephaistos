92e87906802b14ae84f38871e556b01e8e197b67
Nicolas Busseneau <nicolas@busseneau.fr>
2021-05-01 14:14:01 +0200

    cli: refactor subcommands in self-contained units

--- hephaistos.py
+++ hephaistos.py
@@ -7,6 +7,7 @@ resolution using Hor+ scaling (default) or pixel-based scaling.
 Run `hephaistos.py --help` for more information about the available commands.
 """
 
+from abc import ABCMeta, abstractmethod
 from argparse import ArgumentParser
 from enum import Enum
 import logging
@@ -14,7 +15,7 @@ from hashlib import sha256
 from pathlib import Path
 import re
 import sys
-from typing import Tuple
+from typing import NoReturn, Tuple
 
 # Logging
 logging.basicConfig(level=logging.WARNING)
@@ -38,8 +39,19 @@ class Scaling(str, Enum):
     PIXEL_BASED = 'pixel'
 
 
-class PatchHandler:
-    """Handler for the 'patch' subcommand."""
+class SubcommandBase(ArgumentParser, metaclass=ABCMeta):
+    def __init__(self, description: str, **kwargs):
+        super().__init__(add_help=False, **kwargs)
+        self.description = description
+        self.set_defaults(dispatch=self.handler)
+
+    @abstractmethod
+    def handler(self, **kwargs) -> NoReturn:
+        raise NotImplementedError("Subclasses must implement a handler method.")
+
+
+class PatchCommand(SubcommandBase):
+    """`patch` subcommand."""
     # Viewport
     DEFAULT_VIRTUAL_VIEWPORT = (1920, 1080)
 
@@ -49,8 +61,19 @@ class PatchHandler:
     MOV_1920_REGEX = re.compile(b'(\xc7\x05.{4})' + (1920).to_bytes(BYTE_LENGTH, BYTE_ORDER))
     MOV_1080_REGEX = re.compile(b'(\xc7\x05.{4})' + (1080).to_bytes(BYTE_LENGTH, BYTE_ORDER))
     EXPECTED_SUBS = 2    
+
+    def __init__(self, **kwargs):
+        super().__init__(description="patch Hades binaries based on given screen resolution", **kwargs)
+        self.add_argument('width', type=int, help="screen resolution width")
+        self.add_argument('height', type=int, help="screen resolution height")
+        self.add_argument('-s', '--scaling', default=Scaling.HOR_PLUS,
+            choices=[Scaling.HOR_PLUS.value, Scaling.PIXEL_BASED.value],
+            help="scaling type (default: hor+)")
+        self.add_argument('-f', '--force', action='store_true',
+            help="force patching of current binaries, bypassing hash check and replacing previous backups (useful after game update)")
     
-    def __init__(self, width: int, height: int, scaling: Scaling, force: bool, **kwargs):
+    def handler(self, width: int, height: int, scaling: Scaling, force: bool, **kwargs) -> None:
+        """Compute viewport depending on arguments and iterate over DLL paths to patch it in."""
         patch_viewport = self.compute_viewport(width, height, scaling)
         logger.info("Computed patch viewport {} using scaling {}".format(patch_viewport, scaling))
         for engine, dll_filepath in DLL_FILEPATHS.items():
@@ -58,9 +81,9 @@ class PatchHandler:
             self.patch(dll_filepath, patch_viewport, force)
 
     def compute_viewport(self, width: int, height: int, scaling: Scaling) -> Tuple[int, int]:
-        """Compute virtual viewport size to patch depending on scaling type and resolution width / height."""
+        """Compute virtual viewport size to patch depending on scaling type and screen resolution width / height."""
         if scaling == Scaling.HOR_PLUS:
-            (virtual_width, virtual_height) = PatchHandler.DEFAULT_VIRTUAL_VIEWPORT
+            (virtual_width, virtual_height) = PatchCommand.DEFAULT_VIRTUAL_VIEWPORT
             virtual_width = int(width / height * virtual_height)
             return (virtual_width, virtual_height)
         elif scaling == Scaling.PIXEL_BASED:
@@ -72,7 +95,7 @@ class PatchHandler:
         """Patch DLL at filepath with virtual width / height.
 
         On first run:
-        - Store a backup copy of the original DLL for restoration with the 'restore' subcommand.
+        - Store a backup copy of the original DLL for restoration with the `restore` subcommand.
         - Store patched hash in a JSON file (the "hash store").
 
         On subsequent runs, check current DLL hash against previously stored hash:
@@ -80,7 +103,7 @@ class PatchHandler:
         - It not matching, the DLL has changed since the last patch: game was probably updated.
           Patching is stopped so that user may investigate.
         
-        Providing '--force' bypasses the hash check and invalidates previous backups.
+        Providing `--force` bypasses the hash check and invalidates previous backups.
         """
         current_file = Path(dll_filepath)
         current_bytes = current_file.read_bytes()
@@ -115,10 +138,10 @@ class PatchHandler:
             logger.debug("Backed up current {} to {}".format(current_file, backup_file))
 
         # patch and store hash for future reference
-        (hex_width, hex_height) = [(value).to_bytes(PatchHandler.BYTE_LENGTH, PatchHandler.BYTE_ORDER) for value in viewport]
-        (patched_bytes, width_sub_count) = PatchHandler.MOV_1920_REGEX.subn(b'\g<1>' + hex_width, current_bytes)
-        (patched_bytes, height_sub_count) = PatchHandler.MOV_1080_REGEX.subn(b'\g<1>' + hex_height, patched_bytes)
-        if width_sub_count == PatchHandler.EXPECTED_SUBS and height_sub_count == PatchHandler.EXPECTED_SUBS:
+        (hex_width, hex_height) = [(value).to_bytes(PatchCommand.BYTE_LENGTH, PatchCommand.BYTE_ORDER) for value in viewport]
+        (patched_bytes, width_sub_count) = PatchCommand.MOV_1920_REGEX.subn(b'\g<1>' + hex_width, current_bytes)
+        (patched_bytes, height_sub_count) = PatchCommand.MOV_1080_REGEX.subn(b'\g<1>' + hex_height, patched_bytes)
+        if width_sub_count == PatchCommand.EXPECTED_SUBS and height_sub_count == PatchCommand.EXPECTED_SUBS:
             patched_hash = sha256(patched_bytes).hexdigest()
             hash_file.parent.mkdir(parents=True, exist_ok=True)
             hash_file.write_text(patched_hash)
@@ -126,21 +149,25 @@ class PatchHandler:
             current_file.write_bytes(patched_bytes)
             logger.info("Patched {} with viewport {}".format(current_file, viewport))
         else:
-            logger.error("Expected {} matches, found {} for width and {} for height".format(PatchHandler.EXPECTED_SUBS, width_sub_count, height_sub_count))
+            logger.error("Expected {} matches, found {} for width and {} for height".format(PatchCommand.EXPECTED_SUBS, width_sub_count, height_sub_count))
 
 
-class RestoreHandler:
-    """Handler for the 'restore' subcommand."""
+class RestoreCommand(SubcommandBase):
+    """`restore` subcommand."""
     def __init__(self, **kwargs):
+        super().__init__(description="restore Hades binaries from backed up originals", **kwargs)
+    
+    def handler(self, **kwargs) -> None:
+        """Iterate over DLL paths and restore backups."""
         for engine, dll_filepath in DLL_FILEPATHS.items():
             logger.debug("Restoring {} backend".format(engine))
             self.restore(dll_filepath)
 
     def restore(self, dll_filepath: str) -> None:
-        """Restore DLL at filepath from backed up original."""
+        """Restore DLL at filepath from backed up original. Also delete stored hash if any."""
         current_file = Path(dll_filepath)
         backup_file = BACKUP_DIR.joinpath(dll_filepath)
-        # restore backup and invalidate stored hash if any
+        # restore backup and delete stored hash if any
         if backup_file.exists():
             hash_file = HASH_DIR.joinpath(dll_filepath).with_suffix(HASH_EXTENSION)
             hash_file.unlink(missing_ok=True)
@@ -150,43 +177,56 @@ class RestoreHandler:
             logger.error("Could not find backup file {}".format(backup_file))
 
 
-class PrintHelpOnErrorParser(ArgumentParser):
-    """Simple parser printing help when user supplies invalid arguments, rather than leaving them hanging."""
-    def error(self, message):
+class ParserBase(ArgumentParser):
+    """Base parser for hosting shared behavior.
+
+    - Print help when user supplies invalid arguments.
+    - Shared arguments:
+        - Verbosity level: `--verbose` (as repeatable `-v`)
+
+    `ParserBase` serves as the base class for both the main CLI and the actual subcommand parsers.
+    Counter-intuitively, the subcommands defined above MUST NOT directly inherit from `ParserBase`.
+    This is due to how subparsers and parenting work in `argparse`:
+    - When initializing subparsers via `add_subparsers`:
+        - `parser_class` is provided as the base class to use for subcommand parsers.
+    - When adding subparsers via `add_parser`:
+        - A new instance of `parser_class` is instantiated.
+        - If `parents` are provided, the parents' arguments are copied into the new instance.
+        - This instance is the actual parser used for the subcommand.
+    - Parents' types are ignored, and MUST NOT be the same as `parser_class` to avoid argument conflicts.
+    """
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.add_argument('-v', '--verbose', action='count', default=0, help="verbosity level (none: default, '-v': info, '-vv': debug)")
+
+    def error(self, message) -> NoReturn:
+        """Print help when user supplies invalid arguments."""
         sys.stderr.write("error: {}\n\n".format(message))
         self.print_help()
         sys.exit(2)
 
 
+class Hephaistos(ParserBase):
+    """Hephaistos entry point. Main parser for hosting the individual subcommands."""
+    def __init__(self, **kwargs):
+        super().__init__(description="Hephaistos CLI", **kwargs)
+        subparsers = self.add_subparsers(parser_class=ParserBase, required=True, help="one of:", metavar='subcommand', dest='subcommand')
+        subcommands = {
+            'patch': PatchCommand(),
+            'restore': RestoreCommand(),
+        }
+        for name, subcommand in subcommands.items():
+            subparsers.add_parser(name, parents=[subcommand], description=subcommand.description, help=subcommand.description)
+
+        # parse args and dispatch to subcommand via SubcommandBase.dispatch handler
+        args = self.parse_args()
+        if args.verbose:
+            if args.verbose == 1:
+                logger.setLevel(logging.INFO)
+            if args.verbose >= 2:
+                logger.setLevel(logging.DEBUG)
+        args.dispatch(**vars(args))
+
+
 if __name__ == '__main__':
-    # Command line interface
-    parser = PrintHelpOnErrorParser(description="Hephaistos CLI")
-    shared_args = PrintHelpOnErrorParser(add_help=False)
-    shared_args.add_argument('-v', '--verbose', action='count', default=0, help="verbosity level (none: default, '-v': info, '-vv': debug)")
-    subparsers = parser.add_subparsers(required=True, help="one of:", metavar='subcommand', dest='subcommand')
-    
-    # Patch subcommand
-    patch_description = "patch a specific screen resolution in Hades binaries"
-    patch_subcommand = subparsers.add_parser('patch', description=patch_description, parents=[shared_args], help=patch_description)
-    patch_subcommand.add_argument('width', type=int, help="screen width")
-    patch_subcommand.add_argument('height', type=int, help="screen height")
-    patch_subcommand.add_argument('-s', '--scaling', default=Scaling.HOR_PLUS,
-        choices=[Scaling.HOR_PLUS.value, Scaling.PIXEL_BASED.value],
-        help="scaling type (default: hor+)")
-    patch_subcommand.add_argument('-f', '--force', action='store_true',
-        help="force patching of current binaries, bypassing hash check and replacing previous backups (useful after game update)")
-    patch_subcommand.set_defaults(handler=PatchHandler)
-    
-    # Restore subcommand
-    restore_description = "restore Hades binaries from backed up originals"
-    restore_subcommand = subparsers.add_parser('restore', description=restore_description, parents=[shared_args], help=restore_description)
-    restore_subcommand.set_defaults(handler=RestoreHandler)
-    
-    # Parse args, set log level, and execute
-    args = parser.parse_args()
-    if args.verbose:
-        if args.verbose == 1:
-            logger.setLevel(logging.INFO)
-        if args.verbose >= 2:
-            logger.setLevel(logging.DEBUG)
-    args.handler(**vars(args))
+    Hephaistos()
