7a35b1848b5ff1723d143e54fc7387c81d401a4d
Nicolas Busseneau <nicolas@busseneau.fr>
2021-08-21 01:06:16 +0200

    Fix boiling blood animation when getting hit by [Redacted] bloodstone

--- hephaistos-data/lua/Filters/CombatPresentation.lua
+++ hephaistos-data/lua/Filters/CombatPresentation.lua
@@ -1,3 +1,8 @@
+-- boiling blood animation when getting hit by [Redacted] bloodstone
+Hephaistos.CreateAnimation[CastEmbeddedPresentationStart] = function(params)
+	return Hephaistos.MatchAll(params, { Name = "HadesBloodstoneVignette", DestinationId = ScreenAnchors.HadesBloodstoneVignette })
+end
+
 -- poison animation when getting hit by poison
 Hephaistos.CreateAnimation[StartLavaPresentation] = function(params)
 	return Hephaistos.MatchAll(params, { Name = "PoisonVignetteLoop", DestinationId = ScreenAnchors.PoisonVignette })
