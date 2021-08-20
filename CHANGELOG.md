45a33e77859f1fcc602d65213af2efc9c9be822d
Nicolas Busseneau <nicolas@busseneau.fr>
2021-08-20 21:11:58 +0200

    Fix locked keepsakes icons

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Fixed
+
+-   Fix locked keepsakes icons.
+
 ## [v0.8.0] - 2021-08-19
 
 ### Fixed
--- hephaistos-data/lua/Filters/AwardMenuScripts.lua
+++ hephaistos-data/lua/Filters/AwardMenuScripts.lua
@@ -3,7 +3,8 @@ Hephaistos.CreateScreenComponent[ShowAwardMenu] = function(params)
   return Hephaistos.MatchAll(params,
     { Name = "BlankObstacle", Group = "Combat_Menu" },
     { Name = "BlankObstacle", Group = "Combat_Menu_Additive" })
-  and params.X >= 1325
+    and params.X >= 1325
+  or Hephaistos.MatchAll(params, { Name = "LegendaryKeepsakeLockedButton", Group = "Combat_Menu" })
 end
 
 -- keepsakes icons
