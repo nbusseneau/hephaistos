0dd36290dcaade0a50180c8f4c50487d3609544f
Nicolas Busseneau <nicolas@busseneau.fr>
2021-09-14 23:40:00 +0200

    Update main docstring

--- hephaistos/__main__.py
+++ hephaistos/__main__.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 """Hephaistos
 
-CLI tool for patching any resolution in Supergiant Games' Hades, initially
-intended as an ultrawide support mod.
-It can bypass both pillarboxing and letterboxing, which are the default on
-non-16:9 resolutions for Hades.
+CLI tool for patching any resolution in Supergiant Games' Hades, primarily
+targeting ultrawide monitors (21:9, 32:9) and multi-monitors (48:9).
 
-See README for usage examples or run `hephaistos --help` for more information
-about the available commands.
+Hephaistos can bypass both pillarboxing and letterboxing, which are the default
+on non-16:9 resolutions for Hades, and allows using custom resolutions (useful
+for custom window sizes and multi-monitor without Eyefinity / Surround).
+
+See README for usage examples or run:
+
+- `python -m hephaistos` directly to enter interactive mode.
+- `python -m hephaistos --help` for more information about the CLI commands.
 """
 import logging
 
