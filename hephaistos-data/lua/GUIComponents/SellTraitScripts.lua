4a5f6bb5cbc3c258d36bcf01bf64c9d9a2867a5f
Nicolas Busseneau <nicolas@busseneau.fr>
2022-07-06 19:32:16 +0200

    fix pool of purging for vert+

--- hephaistos-data/lua/GUIComponents/SellTraitScripts.lua
+++ hephaistos-data/lua/GUIComponents/SellTraitScripts.lua
@@ -9,6 +9,19 @@ local filters = {
       Action = Hephaistos.Rescale,
     },
   },
+  CreateSellButtons = {
+    -- the boons themselves
+    {
+      Hook = "CreateScreenComponent",
+      Filter = function(params)
+        return Hephaistos.MatchAll(params,
+          { Group = "Combat_Menu" },
+          { Group = "Combat_Menu_Overlay" })
+          and params.X and params.Y
+      end,
+      Action = function(params) params.Y = Hephaistos.RecomputeFixedYFromCenter(params.Y) end,
+    },
+  },
 }
 
 Hephaistos.LoadFilters(filters, Hephaistos.Filters)
