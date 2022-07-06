c28ffedd881ad3df264e55eb6816c1811e1052d4
Nicolas Busseneau <nicolas@busseneau.fr>
2022-07-06 19:29:33 +0200

    fix house contractor up-down scrolls for vert+

--- hephaistos-data/lua/GUIComponents/GhostAdminScreen.lua
+++ hephaistos-data/lua/GUIComponents/GhostAdminScreen.lua
@@ -11,18 +11,24 @@ local filters = {
       end,
       Action = Hephaistos.Rescale,
     },
+    -- house contractor categories
     {
       Hook = "CreateScreenComponent",
       Filter = function(params)
-        -- house contractor categories
         return Hephaistos.MatchAll(params, { Name = "ButtonGhostAdminTab", Y = 245, Scale = 1.0, Sound = "/SFX/Menu Sounds/GeneralWhooshMENU", Group = "Combat_Menu" })
           and params.X >= categoryTitleX
-        -- house contractor up-down scrolls
-        or Hephaistos.MatchAll(params,
-        { Name = "ButtonCodexUp", X = 500, Y = screen.ItemStartY - screen.EntryYSpacer + 75, Scale = 0.75, Sound = "/SFX/Menu Sounds/GeneralWhooshMENU", Group = "Combat_Menu" },
-        { Name = "ButtonContractorDown", X = 500, Y = screen.ItemStartY + ( (screen.EntryYSpacer + 1) * screen.ItemsPerPage) - 75, Scale = 0.75, Sound = "/SFX/Menu Sounds/GeneralWhooshMENU", Group = "Combat_Menu" })
       end,
       Action = Hephaistos.Recenter,
+    },
+    -- house contractor up-down scrolls
+    {
+      Hook = "CreateScreenComponent",
+      Filter = function(params)
+        return Hephaistos.MatchAll(params,
+          { Name = "ButtonCodexUp", X = 500, Y = screen.ItemStartY - screen.EntryYSpacer + 75, Scale = 0.75, Sound = "/SFX/Menu Sounds/GeneralWhooshMENU", Group = "Combat_Menu" },
+          { Name = "ButtonContractorDown", X = 500, Y = screen.ItemStartY + ( (screen.EntryYSpacer + 1) * screen.ItemsPerPage) - 75, Scale = 0.75, Sound = "/SFX/Menu Sounds/GeneralWhooshMENU", Group = "Combat_Menu" })
+      end,
+      Action = function(params) params.X = Hephaistos.RecomputeFixedXFromCenter(params.X) end,
     },
   },
 }
