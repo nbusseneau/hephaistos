088b9c21a73cb216060d577cd239d76d9b9a471b
Nicolas Busseneau <nicolas@busseneau.fr>
2022-07-08 16:27:53 +0200

    fix fated list for vert+

--- hephaistos-data/lua/GUIComponents/QuestLogScreen.lua
+++ hephaistos-data/lua/GUIComponents/QuestLogScreen.lua
@@ -12,16 +12,22 @@ local filterHooks = {
       },
     },
     CreateScreenComponent = {
-      FatedListDescriptionAndScrolls = {
+      -- fated list description box
+      FatedListDescription = {
         Filter = function(params)
-          -- fated list description box
           return Hephaistos.MatchAll(params, { Name = "BlankObstacle", Group = "Combat_Menu", X = 795, Y = 300 })
-            -- fated list up-down scrolls
-            or Hephaistos.MatchAll(params,
-              { Name = "ButtonCodexUp", X = 430, Y = screen.ItemStartY - screen.EntryYSpacer + 1, Scale = 1.0, Sound = "/SFX/Menu Sounds/GeneralWhooshMENU", Group = "Combat_Menu" },
-              { Name = "ButtonCodexDown", X = 430, Y = screen.ItemStartY + ((screen.EntryYSpacer - 1) * screen.ItemsPerPage + 10), Scale = 1.0, Sound = "/SFX/Menu Sounds/GeneralWhooshMENU", Group = "Combat_Menu" })
         end,
         Callback = Hephaistos.Recenter,
+      },
+      -- fated list up-down scrolls
+      FatedListScrolls = {
+        Filter = function(params)
+          -- fated list description box
+          return Hephaistos.MatchAll(params,
+            { Name = "ButtonCodexUp", X = 430, Y = screen.ItemStartY - screen.EntryYSpacer + 1, Scale = 1.0, Sound = "/SFX/Menu Sounds/GeneralWhooshMENU", Group = "Combat_Menu" },
+            { Name = "ButtonCodexDown", X = 430, Y = screen.ItemStartY + ((screen.EntryYSpacer - 1) * screen.ItemsPerPage + 10), Scale = 1.0, Sound = "/SFX/Menu Sounds/GeneralWhooshMENU", Group = "Combat_Menu" })
+        end,
+        Callback = function(params) params.X = Hephaistos.RecomputeFixedXFromCenter(params.X) end,
       },
     },
   },
