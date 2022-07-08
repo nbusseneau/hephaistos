38649c9d99b8af9241de0b6ea93b9b325fd6e197
Nicolas Busseneau <nicolas@busseneau.fr>
2022-07-08 21:23:11 +0200

    fix trait on hover tooltips for vert+

--- hephaistos-data/lua/GUIComponents/TraitTrayScripts.lua
+++ hephaistos-data/lua/GUIComponents/TraitTrayScripts.lua
@@ -16,6 +16,19 @@ local filterHooks = {
       TraitsYCentering = { Callback = function(params) params.Y = Hephaistos.RecomputeFixedYFromCenter(params.Y) end, },
     },
   },
+  SetHighlightedTraitFrame = {
+    -- trait on hover tooltip
+    SetInteractProperty = {
+      TraitOnHoverTooltip = {
+        Filter = function(params)
+          return Hephaistos.MatchAll(params, { Property = "TooltipOffsetY" })
+        end,
+        Callback = function(params)
+          params.Value = Hephaistos.RecomputeFixedYFromCenter(params.Value)
+        end,
+      },
+    },
+  },
 }
 
 Hephaistos.CopyFilterHooks(filterHooks, Hephaistos.FilterHooks)
