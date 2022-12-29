97282fdb6e24f9f049c997aa60c908345d931f23
Nicolas Busseneau <nicolas@busseneau.fr>
2022-12-29 22:13:57 +0100

    add enum types to scaling and hud args

--- hephaistos/cli.py
+++ hephaistos/cli.py
@@ -309,10 +309,10 @@ class PatchSubcommand(BaseSubcommand):
         super().__init__(description="patch Hades using Hephaistos", **kwargs)
         self.add_argument('width', type=int, help="display resolution width")
         self.add_argument('height', type=int, help="display resolution height")
-        self.add_argument('--scaling', default=Scaling.AUTODETECT,
+        self.add_argument('--scaling', type=Scaling, default=Scaling.AUTODETECT,
             choices=[Scaling.HOR_PLUS.value, Scaling.VERT_PLUS.value, Scaling.PIXEL_BASED.value],
             help="scaling algorithm (default: 'hor+' for wider aspect ratios / 'vert+' for taller aspect ratios)")
-        self.add_argument('--hud', default=HUD.AUTODETECT,
+        self.add_argument('--hud', type=HUD, default=HUD.AUTODETECT,
             choices=[HUD.EXPAND.value, HUD.CENTER.value],
             help="HUD mode (default: 'expand' for most aspect ratios / 'center' for 48:9 and wider)")
         self.add_argument('--no-custom-resolution', action='store_false', default=True, dest='custom_resolution',
